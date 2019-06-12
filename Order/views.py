5c704406342d9c235a0fb5b2d1dd2b4a65a5f1b7
FellowCode <31797522+FellowCode@users.noreply.github.com>
2019-06-13 01:52:57 +1000

    update

--- Order/views.py
+++ Order/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import *
 from .models import *
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.http import Http404
 from django.utils import timezone
 from django.db.models import Q
 import googlemaps, os
 from TehPro.settings import BASE_DIR
-
+import threading
+import networkx as nx
 
 def staff_add(request):
     data = {}
@@ -200,16 +201,124 @@ def get_distance_matrix(request):
         for j, col in enumerate(row['elements']):
             dst_matrix['matrix'][i][j]['distance'] = col['distance']['value']
             dst_matrix['matrix'][i][j]['duration'] = col['duration']['value']
-            print('{0}-{1}: dist = {2}, dur = {3}'.format(i, j, col['distance']['value'], col['duration']['value']))
+
 
     return render(request, 'Main/Index.html')
 
+matrix = []
+def get_dist_column(i, origin, distinations, client):
+    result = client.distance_matrix(origin, distinations, language="ru-RU")
+    global matrix
+    matrix[i] = [{} for i in range(len(distinations))]
+    for j, col in enumerate(result['rows'][0]['elements']):
+        matrix[i][j]['distance'] = col['distance']['value']
+        matrix[i][j]['duration'] = col['duration']['value']
+
+def get_dist_mtx(orders):
+    dst_matrix = {}
+    dst_matrix['origins'] = []
+    dst_matrix['orders'] = {}
+    for index, order in enumerate(orders):
+        dst_matrix['orders'][order.id] = index
+        dst_matrix['origins'].append(str(order.client.city) + ', ' + str(order.client.address))
+
+    threads = []
+
+    global matrix
+    matrix = [0 for i in range(len(dst_matrix['origins']))]
+
+    with open(os.path.join(BASE_DIR, 'api_key.txt')) as f:
+        api_key = f.readline().strip()
+    client = googlemaps.Client(api_key)
+
+    for i, origin in enumerate(dst_matrix['origins']):
+        threads.append(threading.Thread(target=get_dist_column, args=(i, origin, dst_matrix['origins'], client)))
+        threads[-1].start()
+
+    for thread in threads:
+        thread.join()
+
+    dst_matrix['matrix'] = matrix
+    return dst_matrix
 
 def division_of_orders(request):
     data = {}
     data['groups'] = Group.objects.all()
 
+
     if request.method == 'POST':
-        groups = request.POST['groups']
+        orders = Order.objects.filter(appointed_time__gte=datetime.now() - timedelta(hours=24))
+        order_time_list = [[] for i in range(6)]
+        for order in orders:
+            index = int((order.appointed_time.astimezone().time().hour - 9) // 2)
+            order_time_list[index].append(order.id)
+
+        dst_matrix = get_dist_mtx(orders)
+        graph = nx.Graph()
+        paths = []
+        lengths = []
+        # создаем ноды
+        for index, elem in enumerate(order_time_list):
+            graph.add_nodes_from(elem)
+        while len(order_time_list) > 0:
+            # print(order_time_list)
+            # удаляем все ребра в графе
+            edges = list(graph.edges)
+            graph.remove_edges_from(edges)
+            # удаляем пустые строки времени (9:00, 11:00...) в списке заявок
+            i = 0
+            while i < len(order_time_list):
+                if len(order_time_list[i]) == 0:
+                    del order_time_list[i]
+                    i -= 1
+                i += 1
+            if len(order_time_list) == 0:
+                break
+            # создаем новые ребра
+            for index, elem in enumerate(order_time_list):
+                if index > 0:
+                    edges = []
+                    for previos_elem in order_time_list[index - 1]:
+                        for current_elem in order_time_list[index]:
+                            previos_index = dst_matrix['orders'][previos_elem]
+                            current_index = dst_matrix['orders'][current_elem]
+                            edges.append((previos_elem, current_elem,
+                                          {'weight': dst_matrix['matrix'][previos_index][current_index]['duration']}))
+                    graph.add_edges_from(edges)
+            # находим кротчайший путь от самой ранней заявки до самой поздней
+            min_length = 99999999999999
+            min_path = []
+            first_elem = order_time_list[0][0]
+            for last_elem in order_time_list[-1]:
+                length = nx.shortest_path_length(graph, source=first_elem, target=last_elem, weight='weight')
+                path = nx.shortest_path(graph, source=first_elem, target=last_elem, weight='weight')
+                if length < min_length:
+                    min_length = length
+                    min_path = path
+            # убираем ноды найденного пути из графа
+            graph.remove_nodes_from(min_path)
+            # добавляем найденный путь в список
+            paths.append(min_path)
+            lengths.append(min_length)
+            # убираем заявки из списка, соответствующие нодам
+            i = 0
+            while i < len(order_time_list):
+                j = 0
+                while j < len(order_time_list[i]):
+                    if order_time_list[i][j] in min_path:
+                        del order_time_list[i][j]
+                        j -= 1
+                    j += 1
+                i += 1
+        groups = request.POST.getlist('groups')
+        print(groups)
+        groups = Group.objects.filter(id__in=groups)
+        data['groups'] = []
+        for index, group in enumerate(groups):
+            if index < len(paths):
+                orders = Order.objects.filter(id__in=paths[index]).order_by('appointed_time')
+                data['groups'].append((group, orders, lengths[index]//60))
+            else:
+                data['groups'].append((group, []))
         return render(request, 'Order/DivisionResult.html', data)
     return render(request, 'Order/DivisionOfOrder.html', data)
